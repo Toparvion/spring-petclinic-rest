414cf88b5cca80c512fed7215dc20358f277845c
Vladimir Plizga <vladimir.plizga@tibbo.com>
2024-07-17 21:28:32 +0700

    Add one more threading sample for long IO

---
+++ src/main/java/org/springframework/samples/petclinic/service/perf/threads/OwnerInfoService.java
@@ -0,0 +1,72 @@
+package org.springframework.samples.petclinic.service.perf.threads;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.util.FileCopyUtils;
+
+import static java.lang.ProcessBuilder.Redirect.INHERIT;
+
+/**
+ * @author Vladimir Plizga
+ */
+@Service
+@ConditionalOnProperty("enable-owner-info")
+public class OwnerInfoService {
+    private static final Logger log = LoggerFactory.getLogger(OwnerInfoService.class);
+
+    public void checkOwnerInfo(String ownerTelephone) {
+        boolean isWindows = System.getProperty("os.name").toLowerCase().contains("windows");
+        String scriptFileName = isWindows
+            ? "getinfo.bat"
+            : "getinfo.sh";
+        Path scriptTempPath = Path.of(System.getProperty("java.io.tmpdir"), scriptFileName);
+        try {
+            InputStream scriptInStream = this.getClass().getResourceAsStream(scriptFileName);
+            Assert.notNull(scriptInStream, "Script '%s' not found in application resources".formatted(scriptFileName));
+
+            OutputStream scriptOutStream = Files.newOutputStream(scriptTempPath);
+            FileCopyUtils.copy(scriptInStream, scriptOutStream);
+            if (!isWindows) {
+                Process chmod = Runtime.getRuntime().exec(new String[] {"chmod", "+x", scriptTempPath.toString()});
+                chmod.waitFor();
+            }
+
+            String[] command = isWindows
+                ? "cmd.exe /c %s %s".formatted(scriptTempPath, ownerTelephone).split(" ")
+//                : "ping -qc 2 localhost".split(" ");
+                : "/bin/sh -c %s %s".formatted(scriptTempPath, ownerTelephone).split(" ");
+            ProcessBuilder pb = new ProcessBuilder(command);
+            pb.redirectError(INHERIT);
+            pb.redirectOutput(INHERIT);
+            Process process = pb.start();
+
+            process.waitFor();
+        }
+        catch (IOException e) {
+            log.error("Failed to check owner info", e);
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                Files.deleteIfExists(scriptTempPath);
+            }
+            catch (IOException e) {
+                log.error("Failed to delete temporary file {}", scriptTempPath, e);
+            }
+        }
+
+    }
+}
---
+++ src/main/resources/org/springframework/samples/petclinic/service/perf/threads/getinfo.sh
@@ -0,0 +1,2 @@
+echo "Checking owner with phone=$1";
+sleep 1
